ae46328b6baec6b4114f68c42c2dcf7c5637feab
mdram83 <michal.dramowicz@gmail.com>
2023-11-04 11:21:02 +0100

    ChatGPT Editor - register initial files

---
+++ wordpress/wp-content/plugins/ppww-chatgpt-editor-plugin/index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Plugin Name:       php probably wrong way - chatgpt editor
+ * Plugin URI:        https://github.com/mdram83/php-probably-wrong-way
+ * Description:       Chat with ChatGPT when creating a content of your page in Editor.
+ * Version:           1.0
+ * Requires at least: 6.3.2
+ * Requires PHP:      8.2
+ * Author:            mdram83
+ * Author URI:        https://github.com/mdram83/
+ * Text Domain:       ppww
+ * Domain Path:       /languages
+ */
